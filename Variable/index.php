7c2c8d02628ecc274e3c69f054af7c2f7def5ef0
kaputhakur <kalpana.ewb@gmail.com>
2022-01-20 16:10:01 +0530

    Add new file

---
+++ Variable/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variables</title>
+</head>
+<h2>Type declration</h2>
+<?php
+$a="kapu";
+$b=&$a;
+$b="helo my name is $b";
+echo"$b";
+
+ echo $_SERVER['PHP_SELF'];
+
+?>
+</body>
+</html>
