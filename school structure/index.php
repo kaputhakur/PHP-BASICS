b6aed50a34a66ffa6d94dca945169e4a62794244
kaputhakur <kalpana.ewb@gmail.com>
2022-03-19 12:01:06 +0530

    676

--- school structure/index.php
+++ school structure/index.php
@@ -10,7 +10,7 @@
 </head>
 
 <body>
-    <h1> School Stucture</h1>
+    <h1 style="text-align:center"> School Stucture</h1>
 
     <?php
     $school =
@@ -23,8 +23,8 @@
                 '10th' =>
                 [
                     'Rooms' => [
-                        ['RoomNo.' => '11', 'Title' => '10th', 'Capicity' => '30', 'Desk' => '15',],
-                        ['RoomNo.' => '13', 'Title' => 'Computer Lab', 'Capicity' => '40', 'Desk' => '40',],
+                        ['RoomNo' => '11', 'Title' => '10th', 'Capicity' => '30', 'Desk' => '15',],
+                        ['RoomNo' => '13', 'Title' => 'Computer Lab', 'Capicity' => '40', 'Desk' => '40',],
                     ],
                     'Subjects' => [
                         ['Name' => 'Science', 'Teacher' => 'Rajeev Kumar', 'Time' => '9:30am-10:10am', 'RoomNo' => '11'],
@@ -812,39 +812,64 @@
             ],
         ];
     ?>
-
-    <table class="table">
-        <thead class="thead-light">
-            <tr>
-
-                <th scope="col">Classes</th>
-                <th scope="col">RoomNO.</th>
-                <th scope="col">Title</th>
-                <th scope="col">Capicity </th>
-                <th scope="col">Desk</th>
-             </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <th scope="col">10th</th>
-
-                <td><?php echo $school['Classes']['10th']['Rooms']['0']['RoomNo.'] ?></td>
-                <td><?php echo $school['Classes']['10th']['Rooms']['0']['Title'] ?></td>
-                <td><?php echo $school['Classes']['10th']['Rooms']['0']['Capicity'] ?></td>
-                <td><?php echo $school['Classes']['10th']['Rooms']['0']['Desk'] ?></td>
-                <td><?php ?></td>
-            </tr>
-
-            <tr>
-                <th scope="row">10th</th>
-                <td><?php echo $school['Classes']['10th']['Rooms']['1']['RoomNo.']  ?></td>
-                <td><?php echo $school['Classes']['10th']['Rooms']['1']['Title'] ?></td>
-                <td><?php echo $school['Classes']['10th']['Rooms']['1']['Capicity'] ?></td>
-                <td><?php echo $school['Classes']['10th']['Rooms']['1']['Desk']?> </td>
-            </tr>
-            
-        </tbody>
-    </table>
+<table class="table">
+  <thead class="thead-dark">
+    <tr>
+      
+      <th scope="col">Classes</th>
+      <th scope="col">RoomNo.</th>
+      <th scope="col">Title</th>
+      <th scope="col">Capicity </th>
+      <th scope="col">Desk</th>
+   
+    </tr>
+  </thead>
+  <tbody>
+      <h3>Rooms</h3>
+    <tr>
+      <th scope="row">10th</th>
+      <td><?php echo $school['Classes']['10th']['Rooms']['0']['RoomNo'] ?></td>
+      <td><?php echo $school['Classes']['10th']['Rooms']['0']['Title'] ?></td>
+      <td><?php echo $school['Classes']['10th']['Rooms']['0']['Capicity'] ?></td>
+      <td><?php echo $school['Classes']['10th']['Rooms']['0']['Desk'] ?></td>
+    </tr>
+    <tr>
+    <th scope="row">10th</th>
+      <td><?php echo $school['Classes']['10th']['Rooms']['1']['RoomNo'] ?></td>
+      <td><?php echo $school['Classes']['10th']['Rooms']['1']['Title'] ?></td>
+      <td><?php echo $school['Classes']['10th']['Rooms']['1']['Capicity'] ?></td>
+      <td><?php echo $school['Classes']['10th']['Rooms']['1']['Desk'] ?></td>
+    </tr>
+   </tbody>
+</table>
+<h3>Subjects</h3>
+<table class="table">
+  <thead class="thead-dark">
+    <tr>
+      
+      <th scope="col">Subjects</th>
+      <th scope="col">Teacher</th>
+      <th scope="col">Time </th>
+      <th scope="col">RoomNo.</th>
+   
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row"><?php echo $school ['Classes']['10th']['Subjects']['0']['Name'] ?></th>
+     <td><?php echo $school ['Classes']['10th']['Subjects']['0']['Teacher']  ?></td>
+      <td><?php echo $school ['Classes']['10th']['Subjects']['0']['Time']  ?></td>
+      <td><?php echo $school ['Classes']['10th']['Subjects']['0']['RoomNo'] ?></td>
+    </tr>
+    <tr>
+    <th scope="row">10th</th>
+      <td><?php echo $school['Classes']['10th']['Rooms']['1']['RoomNo'] ?></td>
+      <td><?php echo $school['Classes']['10th']['Rooms']['1']['Title'] ?></td>
+      <td><?php echo $school['Classes']['10th']['Rooms']['1']['Capicity'] ?></td>
+      <td><?php echo $school['Classes']['10th']['Rooms']['1']['Desk'] ?></td>
+    </tr>
+   </tbody>
+</table>
 
 </body>
 
